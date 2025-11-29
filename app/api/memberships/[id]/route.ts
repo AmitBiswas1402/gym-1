dfd002321e3bb7a0fd2fe24f30490e3cd0103e62
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-11-29 23:33:07 +0530

    Added the edit handler

--- app/api/memberships/[id]/route.ts
+++ app/api/memberships/[id]/route.ts
@@ -1,8 +1,38 @@
 import { connectToDB } from "@/db/db";
 import membershipModel from "@/models/membership.model";
+import { NextRequest } from "next/server";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    await connectToDB();
+
+    const body = await req.json();
+
+    const updatedMember = await membershipModel.findByIdAndUpdate(
+      params.id,
+      body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedMember) {
+      return new Response("Member not found", { status: 404 });
+    }
+
+    return Response.json(updatedMember, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Error updating member", { status: 500 });
+  }
+}
 
 export async function DELETE(
-  req: Request,
+  req: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
